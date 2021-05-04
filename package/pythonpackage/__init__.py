854535114eda29468a9b944458fb0b13c3070d90
Nitesh <niteshkr.dot@gmail.com>
2021-05-04 21:21:36 +0530

    creating a module for python

---
+++ package/pythonpackage/__init__.py
@@ -0,0 +1,6 @@
+class Achha:
+    def __init__(self):
+        print("Constructor ban gaya")
+    def func(self, number):
+        print("This a func function")
+        return number
---
+++ package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name = "pythonpackage",
+version='0.2',
+description="This is easy to use module",
+Long_description="This module makes your code simple and easy.This module makes your code faster",
+author="Someone",
+package=['pythonpackage'],
+install_requires=[])
